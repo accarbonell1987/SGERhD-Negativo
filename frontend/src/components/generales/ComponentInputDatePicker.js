6b546888193f669f7dc49924884121f2810881e1
Alberto Carlos <accarbonell1987@gmail.com>
2020-06-25 14:20:54 -0400

    Fix
    Arreglo en el InputDatePicker

--- frontend/src/components/generales/ComponentInputDatePicker.js
+++ frontend/src/components/generales/ComponentInputDatePicker.js
@@ -13,127 +13,115 @@ import "../global/css/Gestionar.css";
 
 //#region Constantes
 const definitions = {
-  available: { color: null, label: "Disponible" },
-  enquire: { color: "#ffd200", label: "Preguntar" },
-  unavailable: { color: "#78818b", label: "No Disponible" },
+	available: { color: null, label: "Disponible" },
+	enquire: { color: "#ffd200", label: "Preguntar" },
+	unavailable: { color: "#78818b", label: "No Disponible" },
 };
 const ranges = [
-  // {
-  //   state: "enquire",
-  //   range: moment.range(moment().add(2, "weeks").subtract(5, "days"), moment().add(2, "weeks").subtract(6, "days")),
-  // },
-  {
-    state: "unavailable",
-    range: moment.range(moment().subtract(0, "days"), moment().add(100, "years")),
-  },
+	// {
+	//   state: "enquire",
+	//   range: moment.range(moment().add(2, "weeks").subtract(5, "days"), moment().add(2, "weeks").subtract(6, "days")),
+	// },
+	{
+		state: "unavailable",
+		range: moment.range(moment().subtract(0, "days"), moment().add(100, "years")),
+	},
 ];
 //#endregion
 
 //#region Definision de Clase
 class ComponentInputDatePicker extends Component {
-  state = {
-    openModal: false,
-    fecha: null,
-    estados: null,
-    idioma: null,
-    modoboton: false,
-    fechaMinima: null,
-  };
+	state = {
+		openModal: false,
+		fecha: null,
+		estados: null,
+		idioma: null,
+		modoboton: false,
+		fechaMinima: null,
+	};
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.HandleSelect = this.HandleSelect.bind(this);
-    this.ChangeModalState = this.ChangeModalState.bind(this);
-    this.ClearModalState = this.ClearModalState.bind(this);
-  }
+		this.HandleSelect = this.HandleSelect.bind(this);
+		this.ChangeModalState = this.ChangeModalState.bind(this);
+		this.ClearModalState = this.ClearModalState.bind(this);
+	}
 
-  componentDidMount() {
-    this.ClearModalState();
-  }
+	componentDidMount() {
+		this.ClearModalState();
+	}
 
-  HandleSelect = (value, states) => {
-    const today = moment().subtract(1, "days");
-    if (today.isAfter(value)) {
-      this.setState({
-        fecha: value,
-        estados: states,
-        modoboton: true,
-      });
-    }
-  };
-  ChangeModalState = async (evt) => {
-    if (evt.target.className.includes("modal-icon-fire") || evt.target.className.includes("modal-input-100p")) {
-      this.ClearModalState();
-      this.setState({ openModal: true });
-    } else {
-      this.setState({ openModal: false });
-      this.props.SetDate(this.state.fecha);
-    }
-  };
-  ClearModalState = () => {
-    const idioma = moment.locale(es);
-    //actualizar los states
-    this.setState({
-      openModal: false,
-      fecha: this.props.fecha || moment().subtract(1, "days"),
-      estados: null,
-      idioma: idioma,
-      fechaMinima: null,
-    });
-  };
+	HandleSelect = (value, states) => {
+		const today = moment().subtract(1, "days");
+		if (today.isAfter(value)) {
+			this.setState({
+				fecha: value,
+				estados: states,
+				modoboton: true,
+			});
+		}
+	};
+	ChangeModalState = async (evt) => {
+		if (evt.target.className.includes("modal-icon-fire") || evt.target.className.includes("modal-input-100p")) {
+			this.ClearModalState();
+			this.setState({ openModal: true });
+		} else {
+			this.setState({ openModal: false });
+			this.props.SetDate(this.state.fecha);
+		}
+	};
+	ClearModalState = () => {
+		const idioma = moment.locale(es);
+		//actualizar los states
+		this.setState({
+			openModal: false,
+			fecha: this.props.fecha || moment().subtract(1, "days"),
+			estados: null,
+			idioma: idioma,
+			fechaMinima: null,
+		});
+	};
 
-  render() {
-    const rangos = this.props.restringir ? ranges : null;
-    return (
-      <Modal
-        className="modal-datepicker"
-        open={this.state.openModal}
-        trigger={
-          <Input
-            className="modal-input-100p"
-            icon={<Icon name="calendar alternate" className="modal-icon-fire" circular onClick={this.ChangeModalState} />}
-            placeholder="Fecha..."
-            onClick={(evt) => {
-              this.setState({ openModal: true });
-            }}
-            // value={this.state.fecha ? this.state.fecha.format("DD-MM-YYYY") : ""}
-            value={moment(this.state.fecha).format("DD-MM-YYYY")}
-          />
-        }
-      >
-        <Modal.Header icon="calendar alternate">Escoger Fecha</Modal.Header>
-        <Modal.Content>
-          <DateRangePicker
-            firstOfWeek={1}
-            numberOfCalendars={1}
-            selectionType="single"
-            stateDefinitions={definitions}
-            dateStates={rangos}
-            singleDateRange={true}
-            defaultState="available"
-            showLegend={false}
-            locale={this.state.idioma}
-            value={moment(this.state.fecha)}
-            onSelect={this.HandleSelect}
-          />
-        </Modal.Content>
-        <Modal.Actions className="modal-action-centered">
-          {this.state.modoboton ? (
-            <Button color="green" onClick={this.ChangeModalState} className="modal-button-accept" type position>
-              <Icon name="checkmark" className="modal-icon-accept" />
-              Aceptar
-            </Button>
-          ) : (
-            <Button color="red" onClick={this.ChangeModalState} className="modal-button-cancel" type position>
-              <Icon name="remove" className="modal-icon-cancel" />
-              Cerrar
-            </Button>
-          )}
-        </Modal.Actions>
-      </Modal>
-    );
-  }
+	render() {
+		const rangos = this.props.restringir ? ranges : [];
+		return (
+			<Modal
+				className="modal-datepicker"
+				open={this.state.openModal}
+				trigger={
+					<Input
+						className="modal-input-100p"
+						icon={<Icon name="calendar alternate" className="modal-icon-fire" circular onClick={this.ChangeModalState} />}
+						placeholder="Fecha..."
+						onClick={(evt) => {
+							this.setState({ openModal: true });
+						}}
+						// value={this.state.fecha ? this.state.fecha.format("DD-MM-YYYY") : ""}
+						value={moment(this.state.fecha).format("DD-MM-YYYY")}
+					/>
+				}
+			>
+				<Modal.Header icon="calendar alternate">Escoger Fecha</Modal.Header>
+				<Modal.Content>
+					<DateRangePicker firstOfWeek={1} numberOfCalendars={1} selectionType="single" stateDefinitions={definitions} dateStates={rangos} singleDateRange={true} defaultState="available" showLegend={false} locale={this.state.idioma} value={moment(this.state.fecha)} onSelect={this.HandleSelect} />
+				</Modal.Content>
+				<Modal.Actions className="modal-action-centered">
+					{this.state.modoboton ? (
+						<Button color="green" onClick={this.ChangeModalState} className="modal-button-accept" type position>
+							<Icon name="checkmark" className="modal-icon-accept" />
+							Aceptar
+						</Button>
+					) : (
+						<Button color="red" onClick={this.ChangeModalState} className="modal-button-cancel" type position>
+							<Icon name="remove" className="modal-icon-cancel" />
+							Cerrar
+						</Button>
+					)}
+				</Modal.Actions>
+			</Modal>
+		);
+	}
 }
 //#endregion
 
